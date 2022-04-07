1fa06e3e6a8f5d121981ab5031ca9ed8411e3ef2
Mark Clu Heramis <chumheramis@gmail.com>
2022-04-07 14:38:08 +0800

    update error message
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- js/script.js
+++ js/script.js
@@ -18,14 +18,50 @@
 })();
 
 (function () {
+
     $(document).ready(() => {
+        let play = () => {
+            var audio = new Audio('assets/error.mp3');
+            audio.play();
+        }
         var serviceModal1 = new bootstrap.Modal(document.getElementById('service_modal_1'), {
-            keyboard: false
+            keyboard: true
         })
 
         var serviceModalBtn1 = document.getElementById("serviceModalBtn1");
         serviceModalBtn1.addEventListener("click", (e) => {
-            serviceModal1.toggle();
+            play();
+            setTimeout(function () {
+                serviceModal1.toggle();
+            }, 500);
+
+
+        })
+
+        var serviceModal2 = new bootstrap.Modal(document.getElementById('service_modal_2'), {
+            keyboard: true
+        })
+
+        var serviceModalBtn2 = document.getElementById("serviceModalBtn2");
+        serviceModalBtn2.addEventListener("click", (e) => {
+            play();
+            setTimeout(function () {
+                serviceModal2.toggle();
+            }, 500);
+
+
+        })
+
+        var serviceModal3 = new bootstrap.Modal(document.getElementById('service_modal_3'), {
+            keyboard: true
+        })
+
+        var serviceModalBtn3 = document.getElementById("serviceModalBtn3");
+        serviceModalBtn3.addEventListener("click", (e) => {
+            play();
+            setTimeout(function () {
+                serviceModal3.toggle();
+            }, 500);
         })
     })
 
@@ -52,4 +88,6 @@
             .pauseFor(2500)
             .start();
     })
+
+    $('body').scrollspy({ target: '#navbar-example' })
 })()
